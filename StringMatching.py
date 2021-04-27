5ce2178523a2c3c5ceeb019e7e97ec88f752a427
Lock1 <rizaltanur@outlook.com>
2021-04-27 22:51:40 +0700

    Penggabungan csv ke StringMatching
    Penggabungan isi kata penting dan kata tugas menjadi satu pada StringMatching

--- StringMatching.py
+++ StringMatching.py
@@ -1,55 +1,32 @@
-import csv
-impword = []
-tskword = []
+kataPenting = ["kelompok", "deadline", "meet", "mata kuliah", "tambah", "hapus", "ada"]
+kataTugas = ["kuis", "ujian", "tucil", "tubes", "praktikum", "laporan"]
 presuffix = []
-def start(imp, tsk):
-    with open (imp, newline = '') as f:
-        temp = f.read()
-        s = ""
-        for i in temp:
-            if(i == ','):
-                impword.append(s)
-                s = ""
-            else:
-                s += i
-    with open (tsk, newline = '') as f:
-        temp = f.read()
-        s = ""
-        for i in temp:
-            if(i == ','):
-                tskword.append(s)
-                s = ""
-            else:
-                s += i
+
 #Exact String Matching
 def KMP(word, pattern):
-    #print(impword)
-    #print(tskword)
     lenword = len(word)
     lenpattern = len(pattern)
-    print(lenword)
-    print(lenpattern)
     idx1 = idx2 = 0
     presuffix = [0] * lenpattern
-    presuffixx(pattern, lenpattern, presuffix)
+    presuffixx(pattern, presuffix)
+    print(presuffix)
     while idx1 < lenword:
         if pattern[idx2] == word[idx1]:
             idx1 += 1
             idx2 += 1
         if idx2 == lenpattern:
             return True
-            idx2 = presuffix[idx2 - 1]
         elif idx1 < lenword and pattern[idx2] != word[idx1]:
             if idx2 != 0:
                 idx2 = presuffix[idx2 - 1]
             else:
                 idx1 += 1
     return False
-def presuffixx(pattern, lenpattern, presuffix):
+
+def presuffixx(pattern, presuffix):
     temp = 0
-    presuffix[0]
     idx = 1
-    while idx < lenpattern:
+    while idx < len(pattern):
         if pattern[idx] == pattern[temp]:
             temp += 1
             presuffix[idx] = temp
@@ -60,12 +37,13 @@ def presuffixx(pattern, lenpattern, presuffix):
             else:
                 presuffix[idx] = 0
                 idx += 1
+
 #TO DO:
 #Approximation Matching
 #Levenshtein
 
-start("katapenting.csv", "katatugas.csv")
-x = str(input("Kata: " ))
-y = str(input("Pola: "))
+# start("katapenting.csv", "katatugas.csv")
+x = input("Kata: " )
+y = input("Pola: ")
 testKMP = KMP(x, y)
 print(testKMP)
