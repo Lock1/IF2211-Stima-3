7fbbd9d52c3160b42d073182dbe422c240bcfe4c
Lock1 <rizaltanur@outlook.com>
2021-04-28 14:00:40 +0700

    Penambahan rekomendasi
    Penambahan branch untuk merekomendasikan keyword yang tersedia

--- Interface.py
+++ Interface.py
@@ -2,7 +2,7 @@ import StringMatching
 import DateRegex
 import re
 
-keyword = ["hapus", "deadline", "selesai"]
+keyword = ["hapus", "deadline", "selesai", "tambah", "kapan", "apa", "fitur", "bantuan"]
 kataPenting = ["kelompok", "meet", "mata kuliah"]
 kataDurasi = ["bulan", "minggu", "hari"]
 kataTugas = ["kuis", "ujian", "tucil", "tubes", "praktikum", "laporan"]
@@ -23,9 +23,9 @@ def databaseLookup(targetString):
     return None
 
 
-def isArrayRegexFound(sourceArray, targetString):
+def isArrayMatchFound(sourceArray, targetString):
     for pattern in sourceArray:
-        if re.search(pattern, targetString.lower()) != None:
+        if StringMatching.KMP(targetString.lower(), pattern):
             return True
     return False
 
@@ -43,13 +43,15 @@ def isArrayRegexFound(sourceArray, targetString):
 # ["done", Full task string]   -> "selesai" + taskDatabase
 # ["delete", Full task string] -> "hapus" + taskDatabase
 # ["help", None]               -> "bantuan", "fitur"
+# ["search-fail", TaskString]  -> Database lookup failed, TaskString is "add", "see", "delete", etc
+# ["recommend", [Keyword]]     -> No exact keyword match found, [Keyword] list of nearest keyword
 # [None, None]                 -> No matching query
 
 def evaluateString(targetString):
     queryResult = [None, None]
 
     # Adding branch
-    if StringMatching.KMP(targetString.lower(), "tambah") or isArrayRegexFound(kataTugas, targetString):
+    if StringMatching.KMP(targetString.lower(), "tambah") or isArrayMatchFound(kataTugas, targetString):
         tempString = targetString
         for tidakPenting in kataTidakPenting:
             tempString = tempString.replace(tidakPenting, " ")
@@ -61,20 +63,27 @@ def evaluateString(targetString):
 
     # Updating and list branch
     elif StringMatching.KMP(targetString.lower(), "deadline"):
-        # List all deadline
-        if StringMatching.KMP(targetString.lower(), "apa") or StringMatching.KMP(targetString.lower(), "kapan"):
-            # tempString = targetString
-            # for tidakPenting in kataTidakPenting:
-            #     tempString = tempString.replace(tidakPenting, " ")
-            # dateArray = DateRegex.getDate(tempString)
-            # tempString = DateRegex.stripDate(tempString)
-
-            # resultDatabaseQuery = databaseLookup(tempString)
-            # if resultDatabaseQuery != None and dateArray != None:
-            #     database[resultDatabaseQuery[0]][0] = dateArray
+        # List deadline
+        if isArrayMatchFound(kataDurasi, targetString.lower()):
+            # TODO : Specific duration
             queryResult[0] = "see"
             queryResult[1] = database
-                # queryResult[1] = resultDatabaseQuery[1] + " " + DateRegex.dateArrayToString(dateArray)
+
+        elif StringMatching.KMP(targetString.lower(), "kapan"):
+            # Single entry searching
+            resultDatabaseQuery = databaseLookup(targetString.lower())
+            if resultDatabaseQuery != None:
+                queryResult[0] = "see"
+                dateString = DateRegex.dateArrayToString(database[resultDatabaseQuery[0]][0])
+                queryResult[1] = resultDatabaseQuery[1] + " " + DateRegex.dateArrayToString(dateString)
+            else:
+                queryResult[0] = "search-failed"
+                queryResult[1] = "see"
+
+        elif StringMatching.KMP(targetString.lower(), "apa"):
+            # All entry
+            queryResult[0] = "see"
+            queryResult[1] = database
 
         # Updating task
         else:
@@ -89,6 +98,9 @@ def evaluateString(targetString):
                 database[resultDatabaseQuery[0]][0] = dateArray
                 queryResult[0] = "update"
                 queryResult[1] = resultDatabaseQuery[1] + " " + DateRegex.dateArrayToString(dateArray)
+            else:
+                queryResult[0] = "search-failed"
+                queryResult[1] = "update"
 
     # Solved task and delete branch
     elif StringMatching.KMP(targetString.lower(), "selesai") or StringMatching.KMP(targetString.lower(), "hapus"):
@@ -102,16 +114,32 @@ def evaluateString(targetString):
             if StringMatching.KMP(targetString.lower(), "hapus"):
                 queryResult[0] = "delete"
             else:
-                    queryResult[0] = "done"
+                queryResult[0] = "done"
             dateString = DateRegex.dateArrayToString(database[resultDatabaseQuery[0]][0])
             queryResult[1] = resultDatabaseQuery[1] + " " + dateString
             database.pop(resultDatabaseQuery[0])
+        else:
+            queryResult[0] = "search-failed"
+            if StringMatching.KMP(targetString.lower(), "hapus"):
+                queryResult[1] = "delete"
+            else:
+                queryResult[1] = "done"
 
     # Help branch
     elif StringMatching.KMP(targetString.lower(), "fitur") or StringMatching.KMP(targetString.lower(), "bantuan"):
         queryResult[0] = "help"
 
-    # TODO : Recommendation
+    # No keyword match
+    else:
+        availableRecommendation = []
+        for word in keyword:
+            if StringMatching.matchingPercentage(targetString.lower(), word) > 0.6:
+                availableRecommendation.append(word)
+
+        if len(availableRecommendation) > 0:
+            queryResult[0] = "recommend"
+            queryResult[1] = availableRecommendation
+
     return queryResult
 
 
