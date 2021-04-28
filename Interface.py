b7cfca5158626e30debe0d73cd17c23945f0534b
Lock1 <rizaltanur@outlook.com>
2021-04-28 11:08:49 +0700

    Penambahan query untuk add
    Penambahan query yang digunakan untuk menambah task ke database

--- Interface.py
+++ Interface.py
@@ -1,5 +1,6 @@
 import StringMatching
 import DateRegex
+import re
 
 keyword = ["hapus", "deadline", "selesai"]
 kataPenting = ["kelompok", "meet", "mata kuliah"]
@@ -8,10 +9,40 @@ kataTugas = ["kuis", "ujian", "tucil", "tubes", "praktikum", "laporan"]
 kataTidakPenting = ["pada", "tentang", "ke"]
 
 
-    # addTask -> kT + date - kTP, tambah + kT + date
-    # seeTask -> deadline, deadline + kD, deadline + "hari" + "ini", deadline + kT, deadline + date + date
-    # seeSpecific -> deadline + databaseKeyword + "kapan"
-    # updateTask -> deadline + databaseKeyword + date
-    # solvedTask -> selesai + databaseKeyword
-    # help -> give all
-    # deleteTask -> hapus + databaseKeyword
+# ----- evaluateString() return list -----
+# In descending order
+# ["add", Full task string]    -> "tambah", kT + date - kTP
+# ["update", Full task string] -> "deadline" + taskDatabase + date
+# ["done", Full task string]   -> "selesai" + taskDatabase
+# ["delete", Full task string] -> "hapus" + taskDatabase
+# ["see", [Task list]]         -> "deadline"
+# ["help", None]               -> "bantuan", "fitur"
+# [None, None]                 -> No matching query
+
+
+def isArrayRegexFound(sourceArray, targetString):
+    for pattern in sourceArray:
+        if re.search(pattern, targetString.lower()) != None:
+            return True
+    return False
+
+def evaluateString(targetString):
+    resultingQuery = [None, None]
+
+    if re.search("tambah", targetString) != None or isArrayRegexFound(kataTugas, targetString):
+        tempString = targetString
+        for tidakPenting in kataTidakPenting:
+            tempString = tempString.replace(tidakPenting, " ")
+        dateArray = DateRegex.getDate(tempString)
+        if dateArray != None:
+            resultingQuery[0] = "add"
+            resultingQuery[1] = [dateArray, DateRegex.stripDate(tempString)]
+            # TODO : Add to database
+
+    # TODO : Recommendation
+    return resultingQuery
+
+
+# Testing
+temp = input()
+print(evaluateString(temp))
